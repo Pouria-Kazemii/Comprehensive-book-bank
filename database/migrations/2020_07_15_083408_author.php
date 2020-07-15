36b15fc6e9e2e89c450d8560dcd5a5289434cf98
Yamacasis <yamacasis@gmail.com>
2020-07-15 13:10:59 +0430

    Add Author Entity

---
+++ database/migrations/2020_07_15_083408_author.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Author extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('author', function (Blueprint $table) {
+            $table->id();
+            $table->longText("f_name");  // First Name
+            $table->longText("l_name");  // Last Name
+            $table->longText("d_name");  // Display Name
+            $table->longText("country"); // From
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('author');
+    }
+}
