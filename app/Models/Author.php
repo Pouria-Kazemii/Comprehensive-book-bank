db8ac6ff67d216a064b08c2987823f5fd376b042
Yamacasis <yamacasis@gmail.com>
2020-07-18 16:46:54 +0430

    better ignore words and call  clean method

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -9,7 +9,9 @@ use Illuminate\Support\Facades\Log;
 class Author extends Model
 {
     protected $fillable = ['f_name','l_name','d_name', 'country'];
+
     static protected $specialChars = array("؛", ",", "T", ";", "-", "،");
+    static protected $ignorWords = array("به اهتمام", "ترجمه", "تالیف", "نگارش", "مترجم");
 
     public function setAllAttribute($value)
     {
@@ -23,130 +25,13 @@ class Author extends Model
 
         // initial filter
         $authorStr = preg_replace('/[0-9]+/', ' ', $authorStr);
-        $authorStr = str_replace("به اهتمام", ' ', $authorStr);
-        $authorStr = str_replace("ترجمه", ' ', $authorStr);
-        $authorStr = str_replace("تالیف", ' ', $authorStr);
-        $authorStr = str_replace("نگارش", ' ', $authorStr);
-        $authorStr = str_replace("مترجم", ' ', $authorStr);
+        foreach(self::$ignorWords as $ignor){
+            $authorStr = str_replace($ignor, ' ', $authorStr);
+        }
 
         $authArray = array();
-        return Author::specialCharFilterArray($authorStr, $authArray);
-
-
-        $encharsepArray = array();
-        $envcharsepArray = array();
-        $traslatorArray = array();
-        $encharsepArray2 = array();
-        $envcharsepArray2 = array();
-        $traslatorArray2 = array();
-
-        if(strpos($authorStr, "؛") || strpos($authorStr, ";") || strpos($authorStr, "T") || strpos($authorStr, ",")){
-            $authArray = explode("؛", $authorStr);
-            $encharsepArray = explode(";", $authorStr);
-            $envcharsepArray = explode(",", $authorStr);
-            $traslatorArray = explode("T", $authorStr);
-
-
-            foreach($authArray as $authKey =>&$auth){
-                if(strpos($auth, ";")){
-                    $encharsepArray2 = explode(";", $auth);
-                    unset($authArray[$authKey]);
-                }
-                if(strpos($auth, "T")){
-                    $traslatorArray2 = explode("T", $auth);
-                    unset($authArray[$authKey]);
-                }
-                if(strpos($auth, ",")){
-                    $envcharsepArray2 = explode(",", $auth);
-                    unset($authArray[$authKey]);
-                }
-                $auth.="-A-";
-            }
-
-            foreach($traslatorArray as $authKey =>&$auth){
-                if(strpos($auth, ";")){
-                    unset($traslatorArray[$authKey]);
-                }
-                if(strpos($auth, "T")){
-                    unset($traslatorArray[$authKey]);
-                }
-                if(strpos($auth, ",")){
-                    unset($traslatorArray[$authKey]);
-                }
-                $auth.="-B-";
-            }
-            foreach($encharsepArray as $authKey =>&$auth){
-                if(strpos($auth, ";")){
-                    unset($encharsepArray[$authKey]);
-                }
-                if(strpos($auth, "T")){
-                    unset($encharsepArray[$authKey]);
-                }
-                if(strpos($auth, ",")){
-                    unset($encharsepArray[$authKey]);
-                }
-                $auth.="-C-";
-            }
-            foreach($envcharsepArray as $authKey =>&$auth){
-                if(strpos($auth, ";")){
-                    unset($envcharsepArray[$authKey]);
-                }
-                if(strpos($auth, "T")){
-                    unset($envcharsepArray[$authKey]);
-                }
-                if(strpos($auth, ",")){
-                    unset($envcharsepArray[$authKey]);
-                }
-                $auth.="-D-";
-            }
-
-            foreach($traslatorArray2 as $authKey =>&$auth){
-                if(strpos($auth, ";")){
-                    unset($traslatorArray2[$authKey]);
-                }
-                if(strpos($auth, "T")){
-                    unset($traslatorArray2[$authKey]);
-                }
-                if(strpos($auth, ",")){
-                    unset($traslatorArray2[$authKey]);
-                }
-                $auth.="-E-";
-            }
-            foreach($encharsepArray2 as $authKey =>&$auth){
-                if(strpos($auth, ";")){
-                    unset($encharsepArray2[$authKey]);
-                }
-                if(strpos($auth, "T")){
-                    unset($encharsepArray2[$authKey]);
-                }
-                if(strpos($auth, ",")){
-                    unset($encharsepArray2[$authKey]);
-                }
-                $auth.="-F-";
-            }
-            foreach($envcharsepArray2 as $authKey =>&$auth){
-                if(strpos($auth, ";")){
-                    unset($envcharsepArray2[$authKey]);
-                }
-                if(strpos($auth, "T")){
-                    unset($envcharsepArray2[$authKey]);
-                }
-                if(strpos($auth, ",")){
-                    unset($envcharsepArray2[$authKey]);
-                }
-            }
-
-            $authArray = array_merge($authArray, $traslatorArray, $encharsepArray, $traslatorArray2, $encharsepArray2, $envcharsepArray2);
-
-            foreach($authArray as &$auth){
-                if(strpos($auth, "،")){
-                    $authNames = explode("،" , $auth);
-                    $auth = $authNames[1]." ".$authNames[0];
-                }
-
-                $auth = trim($auth);
-            }
-        }
+        $authArray = Author::specialCharFilterArray($authorStr, $authArray);
+        $authArray = Author::specialCharCleanerArray($authArray);
 
 
         return array_unique($authArray);
