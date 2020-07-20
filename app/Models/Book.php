ff82d700338c67977c326438540b21286c99d887
Yamacasis <yamacasis@gmail.com>
2020-07-20 13:10:25 +0430

    unique output array

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -39,7 +39,7 @@ class Book extends Model
             }
         }
         if(count($shabakArray)==0)$shabakArray[]=(int)($shabakStr);
-        return $shabakArray;
+        return array_unique($shabakArray);
     }
 
 }
