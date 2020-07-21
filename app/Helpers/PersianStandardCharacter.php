c3949935a6d003451e3e4767376437ce2c267194
Yamacasis <yamacasis@gmail.com>
2020-07-21 09:18:09 +0430

    fix missing character

--- app/Helpers/PersianStandardCharacter.php
+++ app/Helpers/PersianStandardCharacter.php
@@ -89,7 +89,7 @@ if (!function_exists('cleanFaAlphabet')) {
      */
     function cleanFaAlphabet($faStr)
     {
-        $faAlphabet = explode(' ', 'ا ب پ ت ث ج چ ح خ د ذ ر ز ژ ص ض ط ظ ع غ ف ق ک گ ل م ن و ه ی [ ] { } . , ; . ، |');
+        $faAlphabet = explode(' ', 'ا ب پ ت ث ج چ ح خ د ذ ر ز ژ س ش ص ض ط ظ ع غ ف ق ک گ ل م ن و ه ی ئ ء [ ] { } . , ; . ، |');
         $faStr = arCharToFA($faStr);
         foreach($faAlphabet as $fa){
             $faStr = str_replace($fa, "", $faStr);
