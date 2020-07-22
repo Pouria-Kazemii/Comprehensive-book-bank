f5ac1aa5da769aa5102ff1b44b5e5297331afe85
Yamacasis <yamacasis@gmail.com>
2020-07-22 12:02:50 +0430

    api routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::middleware(['CheckToken'])->group(function () {
+    Route::post('/api/v1/find/book','API\Book@find');
+    Route::post('/api/v1/find/library','API\Library@find');
 });
