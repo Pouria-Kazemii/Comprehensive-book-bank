b1950a9fe7dde33af57eece32390bbd98343bece
Yamacasis <yamacasis@gmail.com>
2020-07-25 14:56:40 +0430

    fix null problems

--- app/Http/Controllers/API/Book.php
+++ app/Http/Controllers/API/Book.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\Book as BookM;
+use Hamcrest\Type\IsObject;
 
 class Book extends Controller
 {
@@ -14,8 +15,7 @@ class Book extends Controller
 
         if ($request->input('shabak','') != ''){
             $books = BookM::where('shabak',$request->input('shabak'))->first();
-        }
-        if(!isset($books)){
+        }else{
             if($request->input('title','')!=''){
                 $qBooks = BookM::with(['authors', 'libraries'])->where('Title','LIKE','%'.$request->input('title').'%');
             }
@@ -35,40 +35,43 @@ class Book extends Controller
             $books= $qBooks->orderBy('lastCheckLibraries', 'desc')->take(10)->get();
         }
         $resultArray = array();
-        foreach($books as $book){
-            $temp['title'] = $book->Title;
-            $temp['nasher'] = $book->Nasher;
-            $temp['shabak'] = $book->shabak;
-            $temp['barcode'] = $book->barcode;
-            $temp['mozoe'] = $book->mozoe;
-            $temp['salenashr'] = $book->saleNashr;
-            $temp['mahalnashr'] = $book->MahalNashr;
-            $temp['tedadsafhe'] = $book->TedadSafhe;
-            $temp['image'] = $book->Image_Address;
-            $temp['lang'] = $book->langName;
-            $temp['radeasliD'] = $book->RadeAsliD;
-            $temp['radefareiD'] = $book->RadeFareiD;
-            $temp['katerD'] = $book->ShomareKaterD;
-            $temp['pishrade'] = $book->PishRade;
+        if(IsObject($books)){
+            foreach($books as $book){
+                $temp['title'] = $book->Title;
+                $temp['nasher'] = $book->Nasher;
+                $temp['shabak'] = $book->shabak;
+                $temp['barcode'] = $book->barcode;
+                $temp['mozoe'] = $book->mozoe;
+                $temp['salenashr'] = $book->saleNashr;
+                $temp['mahalnashr'] = $book->MahalNashr;
+                $temp['tedadsafhe'] = $book->TedadSafhe;
+                $temp['image'] = $book->Image_Address;
+                $temp['lang'] = $book->langName;
+                $temp['radeasliD'] = $book->RadeAsliD;
+                $temp['radefareiD'] = $book->RadeFareiD;
+                $temp['katerD'] = $book->ShomareKaterD;
+                $temp['pishrade'] = $book->PishRade;
 
-            foreach($book->authors as $author){
-                $temp['authors'][] = $author->d_name;
+                foreach($book->authors as $author){
+                    $temp['authors'][] = $author->d_name;
+                }
+                foreach($book->libraries as $key=>$library){
+                    $temp['libraries'][$key]['code']      = $library->libraryCode;
+                    $temp['libraries'][$key]['name']      = $library->libraryName;
+                    $temp['libraries'][$key]['address']   = $library->address;
+                    $temp['libraries'][$key]['postcode']  = $library->postCode;
+                    $temp['libraries'][$key]['phone']     = $library->phone;
+                    $temp['libraries'][$key]['state']     = $library->state();
+                    $temp['libraries'][$key]['city']      = $library->city();
+                }
+
+
+                $resultArray[] = $temp;
+
+
             }
-            foreach($book->libraries as $key=>$library){
-                $temp['libraries'][$key]['code']      = $library->libraryCode;
-                $temp['libraries'][$key]['name']      = $library->libraryName;
-                $temp['libraries'][$key]['address']   = $library->address;
-                $temp['libraries'][$key]['postcode']  = $library->postCode;
-                $temp['libraries'][$key]['phone']     = $library->phone;
-                $temp['libraries'][$key]['state']     = $library->state();
-                $temp['libraries'][$key]['city']      = $library->city();
-            }
-
-
-            $resultArray[] = $temp;
-
-
         }
+
 
         $resultCount = count($resultArray);
         if($resultCount == 0){
