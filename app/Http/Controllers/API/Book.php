d1cb41b93cbc75cbdb0967178215ce79d160b89f
Yamacasis <yamacasis@gmail.com>
2020-07-25 12:06:07 +0430

    compelet find function

--- app/Http/Controllers/API/Book.php
+++ app/Http/Controllers/API/Book.php
@@ -4,11 +4,32 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Book as BookM;
 
 class Book extends Controller
 {
     //
-    public function find(){
-        echo "Book";
+    public function find(Request $request){
+
+        if ($request->input('shabak','') != ''){
+            $qBooks = BookM::where('shabak',$request->input('shabak'));
+        }
+        if($request->input('title','')!=''){
+            $qBooks = BookM::where('Title','LIKE','%'.$request->input('title').'%');
+        }
+        if($request->input('nasher','')!=''){
+            $qBooks->orWhere('nasher','LIKE','%'.$request->input('nasher').'%');
+        }
+        if($request->input('mahalenashr','')!=''){
+            $qBooks->orWhere('MahalNashr','LIKE','%'.$request->input('mahalenashr').'%');
+        }
+        if($request->input('salenashr','')!=''){
+            $qBooks->orWhere('saleNashr','LIKE','%'.$request->input('salenashr').'%');
+        }
+        if($request->input('tedadsafe','')!=''){
+            $qBooks->orWhere('TedadSafhe','LIKE','%'.$request->input('tedadsafe').'%');
+        }
+
+        print_r($qBooks->orderBy('lastCheckLibraries', 'desc')->take(10)->get());
     }
 }
