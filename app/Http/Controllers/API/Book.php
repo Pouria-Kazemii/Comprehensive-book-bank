82ac7b8073b32af87e945fe9a13e03cee8eaf21f
Yamacasis <yamacasis@gmail.com>
2020-07-25 15:20:55 +0430

    search two way for find

--- app/Http/Controllers/API/Book.php
+++ app/Http/Controllers/API/Book.php
@@ -15,7 +15,8 @@ class Book extends Controller
 
         if ($request->input('shabak','') != ''){
             $books = BookM::where('shabak',$request->input('shabak'))->first();
-        }else{
+        }
+        if(!is_object($books)){
             if($request->input('title','')!=''){
                 $qBooks = BookM::with(['authors', 'libraries'])->where('Title','LIKE','%'.$request->input('title').'%');
             }
