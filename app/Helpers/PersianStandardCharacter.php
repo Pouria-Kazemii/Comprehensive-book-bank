1432769a5fe7bf326edaa49edf3ed13e50d5cbdd
Yamacasis <yamacasis@gmail.com>
2020-07-29 14:15:06 +0430

    add English Number Keep Only

--- app/Helpers/PersianStandardCharacter.php
+++ app/Helpers/PersianStandardCharacter.php
@@ -121,3 +121,16 @@ if (!function_exists('faAlphabetKeep')) {
         return trim($faStr);
     }
 }
+if (!function_exists('enNumberKeepOnly')) {
+
+    /**
+     * Keep fa alphabet only
+     *
+     * @param string $numallStr
+     * @return string $cleanedStr
+     */
+    function enNumberKeepOnly($numallStr)
+    {
+        return trim(preg_replace("/[^0-9]/", "",$numallStr));
+    }
+}
