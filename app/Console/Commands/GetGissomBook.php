c39ff3692398f7c7c0f2869ec915b1d7e5fba945
Yamacasis <yamacasis@gmail.com>
2020-08-09 16:29:09 +0430

    test gisoom book

--- app/Console/Commands/GetGissomBook.php
+++ app/Console/Commands/GetGissomBook.php
@@ -40,8 +40,69 @@ class GetGissomBook extends Command
     public function handle()
     {
         $client = new Client(HttpClient::create(['timeout' => 30]));
+        $recordNumber = 11000000;
         $crawler = $client->request('GET', 'https://www.gisoom.com/book/11000000/');
-        print_r($crawler);
+
+        // title $crawler->filter('body div.bookinfocol div h1 a')[0]->textContent)
+        // all data $crawler->filter('body div.bookinfocol div.col'))
+        // image $crawler->filter('body img.cls3')
+        $filtered= array();
+        $title = $crawler->filter('body div.bookinfocol div h1 a');
+        $filtered['title'] = $title->text();
+
+        foreach($crawler->filter('body div.bookinfocol div.col') as $col){
+            if(strpos($col->textContent, 'ناشر:') !== false){
+                $filtered['nasher'] = $col->filter('a')->text();
+            }
+            if(strpos($col->textContent, 'ویراستار:') !== false){
+                $filtered['editor'] = $col->filter('a')->text();
+            }
+            if(strpos($col->textContent, 'مترجمان:') !== false){
+                $filtered['tarjome'] = true;
+
+            }
+            if(strpos($col->textContent, 'مترجمان:') !== false || strpos($col->textContent, 'مؤلف:') !== false ){
+                foreach($col->filter('a') as $author){
+                    $autors[] = $author->text();
+                }
+            }
+            if(strpos($col->textContent, 'زبان:') !== false){
+                $filtered['lang'] = str_replace('زبان:','',$col->textContent);
+            }
+            if(strpos($col->textContent, 'رده‌بندی دیویی:') !== false){
+                $filtered['radeD'] = str_replace('رده‌بندی دیویی:','',$col->textContent);
+            }
+            if(strpos($col->textContent, 'سال چاپ:') !== false){
+                $filtered['saleNashr'] = str_replace('سال چاپ:','',$col->textContent);
+            }
+            if(strpos($col->textContent, 'نوبت چاپ:') !== false){
+                $filtered['nobatChap'] = str_replace('نوبت چاپ:','',$col->textContent);
+            }
+            if(strpos($col->textContent, 'تیراژ:') !== false){
+                $filtered['tiraj'] = str_replace('تیراژ:','',$col->textContent);
+            }
+            if(strpos($col->textContent, 'تعداد صفحات:') !== false){
+                $filtered['tedadSafe'] = str_replace('تعداد صفحات:','',$col->textContent);
+            }
+            if(strpos($col->textContent, 'قطع و نوع جلد:') !== false){
+                $filtered['ghateChap'] = str_replace('قطع و نوع جلد:','',$col->textContent);
+            }
+            if(strpos($col->textContent, 'شابک 10 رقمی:') !== false){
+                $filtered['shabak10'] = str_replace('شابک 10 رقمی:','',$col->textContent);
+            }
+            if(strpos($col->textContent, 'شابک 13 رقمی:') !== false){
+                $filtered['shabak13'] = str_replace('شابک 13 رقمی:','',$col->textContent);
+            }
+            if(strpos($col->textContent, 'توضیح کتاب:') !== false){
+                $filtered['desc'] = $col->filter('div.justify')->text();
+            }
+
+
+        }
+        $filtered['recordNumber'] = $recordNumber;
+
+
+        print_r($recordNumber);
         exit;
     }
 }
