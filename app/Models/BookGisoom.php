488a9d16342de2d3865075041b77a4e6d5cf64f7
Yamacasis <yamacasis@gmail.com>
2020-08-10 12:30:17 +0430

    add all gisoom book field to model

--- app/Models/BookGisoom.php
+++ app/Models/BookGisoom.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class BookGisoom extends Model
 {
-    protected $fillable = ['title','nasher','lang','editor','radeD','saleNashr','nobatChap','tiraj', 'tedadSafe', 'ghateChap', 'shabak10', 'shabak13','recordNumber','tarjome','desc','image'];
+    protected $fillable = ['title','nasher','lang','editor','radeD','saleNashr','nobatChap','tiraj', 'tedadSafe', 'ghateChap', 'shabak10', 'shabak13','recordNumber','tarjome','desc','image','price','bigTitle','nnumber','sarshenase','nasherDesc','zaherDesc','descriptions','catText','radeKongere','radeDText','relatedN'];
     protected $table = 'bookgisoom';
 
     public function authors() {
