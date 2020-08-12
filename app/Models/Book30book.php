fe913fab1dd674b951cbb0530ca11cb8074bc5e4
Yamacasis <yamacasis@gmail.com>
2020-08-12 15:23:36 +0430

    create 30book model

---
+++ app/Models/Book30book.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Library\Library;
+use Illuminate\Database\Eloquent\Model;
+
+class Book30book extends Model
+{
+    protected $fillable = ['title','nasher','lang','cats','saleNashr','nobatChap', 'tedadSafe', 'ghateChap', 'jeld', 'shabak', 'vazn','catPath','tarjome','desc','image','price'];
+    protected $table = 'book30book';
+
+    public function authors() {
+        return $this->belongsToMany(Author::class);
+    }
+}
---
+++ database/migrations/2020_08_12_104309_create_30book_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Create30bookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book30book', function (Blueprint $table) {
+            $table->id();
+            $table->integer('recordNumber')->index();
+            $table->longText('title')->nullable();
+            $table->longText('lang')->nullable();
+            $table->longText('shabak')->nullable();
+            $table->longText('cats')->nullable();
+            $table->longText('nasher')->nullable();
+            $table->integer('saleNashr')->nullable();
+            $table->integer('nobatChap')->nullable();
+            $table->integer('tedadSafe')->nullable();
+            $table->longText('ghatechap')->nullable();
+            $table->boolean('tarjome')->nullable()->default(false);
+            $table->binary('desc')->nullable();
+            $table->longText('jeld')->nullable();
+            $table->integer('vazn')->nullable()->default(0);
+            $table->longText('image')->nullable();
+            $table->longText('catPath')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book30book');
+    }
+}
