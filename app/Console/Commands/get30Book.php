8e9ce56803a1711c0c5a8005125fab4115631743
Yamacasis <yamacasis@gmail.com>
2020-08-12 09:41:30 +0430

    ignore game cat

--- app/Console/Commands/get30Book.php
+++ app/Console/Commands/get30Book.php
@@ -42,83 +42,91 @@ class get30Book extends Command
     {
         $client = new Client(HttpClient::create(['timeout' => 30]));
         $recordNumber = $this->argument('Id');
+
         try {
             $this->info(" \n ---------- Try Get BOOK ".$recordNumber."              ---------- ");
             $crawler = $client->request('GET', 'https://www.30book.com/Book/'.$recordNumber);
             $status_code = $client->getInternalResponse()->getStatusCode();
-        } catch (\Exception $e) {
+        }catch (\Exception $e) {
             $crawler = null;
             $status_code = 500;
             $this->info(" \n ---------- Failed Get  ".$recordNumber."              ---------=-- ");
         }
+
         if($status_code == 200){
+
             $filtered= array();
-            $filtered['title']  = $crawler->filter('body div.body-content h1')->text();
-            $filtered['nasher'] = $crawler->filter('body div.body-content h2 a.site-c')->text();
-            $filtered['price']  = enNumberKeepOnly(faCharToEN($crawler->filter('body div.body-content span.price-slash')->text()));
-            $filtered['image']  = 'https://www.30book.com/'.$crawler->filter('body div.body-content div.card img.rounded')->attr('src');
-            $filtered['desc']  = $crawler->filter('body div.body-content p.line-h-2')->text('');
 
             foreach ($crawler->filter('body div.body-content a.indigo') as $cat){
                 if(isset($filtered['cats']))$filtered['cats']= $filtered['cats']."-|-".$cat->textContent;
                 else $filtered['cats']= $cat->textContent;
             }
-            $authors = array();
-            foreach ($crawler->filter("body div.body-content table.table-striped tr") as $trTable){
-                $trObj = new Crawler($trTable);
-                switch($trObj->filter('td')->first()->text('')){
-                    case 'شابک':
-                        $filtered['shabak'] = $trObj->filter('td')->nextAll()->text('');
-                    break;
-                    case 'نویسنده':
-                        if($trObj->filter('td')->nextAll()->text('') != ''){
-                            foreach($trObj->filter('a') as $link){
-                                $authors[] = $link->textContent;
+            if(!in_array('بازی و اسباب بازی',$filtered['cats'])){
+
+                $filtered['title']  = $crawler->filter('body div.body-content h1')->text();
+                $filtered['nasher'] = $crawler->filter('body div.body-content h2 a.site-c')->text();
+                $filtered['price']  = enNumberKeepOnly(faCharToEN($crawler->filter('body div.body-content span.price-slash')->text()));
+                $filtered['image']  = 'https://www.30book.com/'.$crawler->filter('body div.body-content div.card img.rounded')->attr('src');
+                $filtered['desc']  = $crawler->filter('body div.body-content p.line-h-2')->text('');
+
+
+                $authors = array();
+                foreach ($crawler->filter("body div.body-content table.table-striped tr") as $trTable){
+                    $trObj = new Crawler($trTable);
+                    switch($trObj->filter('td')->first()->text('')){
+                        case 'شابک':
+                            $filtered['shabak'] = $trObj->filter('td')->nextAll()->text('');
+                        break;
+                        case 'نویسنده':
+                            if($trObj->filter('td')->nextAll()->text('') != ''){
+                                foreach($trObj->filter('a') as $link){
+                                    $authors[] = $link->textContent;
+                                }
                             }
-                        }
-                    break;
-                    case 'مترجم':
-                        if($trObj->filter('td')->nextAll()->text('') != ''){
-                            $filtered['tarjome'] = true;
-                            foreach($trObj->filter('a') as $link){
-                                $authors[] = $link->textContent;
+                        break;
+                        case 'مترجم':
+                            if($trObj->filter('td')->nextAll()->text('') != ''){
+                                $filtered['tarjome'] = true;
+                                foreach($trObj->filter('a') as $link){
+                                    $authors[] = $link->textContent;
+                                }
                             }
-                        }
-                    break;
-                    case 'سال انتشار':
-                        $filtered['saleNashr'] = $trObj->filter('td')->nextAll()->text('');
-                    break;
-                    case 'نوبت چاپ':
-                        $filtered['nobatChap'] = $trObj->filter('td')->nextAll()->text('');
-                    break;
-                    case 'زبان کتاب':
-                        $filtered['lang'] = $trObj->filter('td')->nextAll()->text('');
-                    break;
-                    case 'قطع کتاب':
-                        $filtered['ghateChap'] = $trObj->filter('td')->nextAll()->text('');
-                    break;
-                    case 'جلد کتاب':
-                        $filtered['jeld'] = $trObj->filter('td')->nextAll()->text('');
-                    break;
-                    case 'تعداد صفحه':
-                        $filtered['tedadSafe'] = $trObj->filter('td')->nextAll()->text('');
-                    break;
-                    case 'وزن':
-                        $filtered['vazn'] = $trObj->filter('td')->nextAll()->text('');
-                    break;
+                        break;
+                        case 'سال انتشار':
+                            $filtered['saleNashr'] = $trObj->filter('td')->nextAll()->text('');
+                        break;
+                        case 'نوبت چاپ':
+                            $filtered['nobatChap'] = $trObj->filter('td')->nextAll()->text('');
+                        break;
+                        case 'زبان کتاب':
+                            $filtered['lang'] = $trObj->filter('td')->nextAll()->text('');
+                        break;
+                        case 'قطع کتاب':
+                            $filtered['ghateChap'] = $trObj->filter('td')->nextAll()->text('');
+                        break;
+                        case 'جلد کتاب':
+                            $filtered['jeld'] = $trObj->filter('td')->nextAll()->text('');
+                        break;
+                        case 'تعداد صفحه':
+                            $filtered['tedadSafe'] = $trObj->filter('td')->nextAll()->text('');
+                        break;
+                        case 'وزن':
+                            $filtered['vazn'] = $trObj->filter('td')->nextAll()->text('');
+                        break;
 
+                    }
                 }
-            }
 
-            foreach ($crawler->filter("body div.body-content li.breadcrumb-item a") as $linkcat){
-                if($linkcat->textContent != 'خانه'){
-                    if(isset($filtered['catPath']))$filtered['catPath'] = $filtered['catPath']."-|-".$linkcat->textContent;
-                    else $filtered['catPath'] = $linkcat->textContent;
+                foreach ($crawler->filter("body div.body-content li.breadcrumb-item a") as $linkcat){
+                    if($linkcat->textContent != 'خانه'){
+                        if(isset($filtered['catPath']))$filtered['catPath'] = $filtered['catPath']."-|-".$linkcat->textContent;
+                        else $filtered['catPath'] = $linkcat->textContent;
+                    }
                 }
-            }
 
-            print_r($authors);
-            print_r($filtered);
+                print_r($authors);
+                print_r($filtered);
+            }
         }
     }
 }
