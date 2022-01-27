fc93ea83a380dac09221cff14ce044735bad44c1
root <yamacasis@yahoo.com>
2022-01-27 05:27:25 +0000

    Remove MongoDB driverClass App\Models\User located in ./app/User.php does not comply with psr-4 autoloading standard. Skipping.

--- config/app.php
+++ config/app.php
@@ -176,11 +176,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        /*
-         * Jenssegers Mongodb Providers...
-         */
-        Jenssegers\Mongodb\MongodbServiceProvider::class,
-
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
     ],
 
