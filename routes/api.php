f195b8d86b9e5dfac0493c04a80ff7148013ea60
mohan21 <firm_mohan21@yahoo.com>
2022-01-30 19:12:38 +0330

    fix cors 18

--- routes/api.php
+++ routes/api.php
@@ -19,12 +19,14 @@ Route::post('/v1/user/login', 'API\UserController@login');
 //Route::get('/v1/book/find', 'API\BookController@find');
 //Route::get('/v1/book/check', 'API\BookController@checkBookK24');
 
-Route::group
-(
-    ['middleware' => ['jwt.auth']],
-    function()
-    {
-        Route::post('/v1/book/find', 'API\BookController@find');
-        Route::post('/v1/book/detail', 'API\BookController@detail');
-    }
-);
+//Route::group
+//(
+//    ['middleware' => ['jwt.auth']],
+//    function()
+//    {
+//        Route::post('/v1/book/find', 'API\BookController@find');
+//        Route::post('/v1/book/detail', 'API\BookController@detail');
+//    }
+//);
+Route::post('/v1/book/find', 'API\BookController@find');
+Route::post('/v1/book/detail', 'API\BookController@detail');
