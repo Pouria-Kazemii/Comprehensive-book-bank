0e15686ec9ae29e1a4234b3f32e99c359a0d0d8a
mohan21 <firm_mohan21@yahoo.com>
2022-02-03 13:15:25 +0330

    test find book

--- app/Http/Controllers/API/BookController.php
+++ app/Http/Controllers/API/BookController.php
@@ -35,7 +35,7 @@ class BookController extends Controller
         $totalPages = 0;
         $totalRows = 50;
         $offset = ($currentPageNumber - 1) * $pageRows;
-/*
+
         // read books
         $books = BookirBook::orderBy('xpublishdate', 'desc');
         if($name != "") $books->where('xname', 'like', "%$name%");
@@ -114,7 +114,7 @@ class BookController extends Controller
         if($isbn != "") $books->where('xisbn', '=', $isbn);
         $totalRows = $books->count();
         $totalPages = $totalRows > 0 ? (int) ceil($totalRows / $pageRows) : 0;
-*/
+
         // response
         return response()->json
         (
