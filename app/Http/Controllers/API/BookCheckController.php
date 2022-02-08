f2cdb533efb6ae141baeb3aea003edefe5af5f34
mohan21 <firm_mohan21@yahoo.com>
2022-02-08 12:57:04 +0330

    edit book check

--- app/Http/Controllers/API/BookCheckController.php
+++ app/Http/Controllers/API/BookCheckController.php
@@ -11,7 +11,7 @@ class BookCheckController extends Controller
 {
     public function check()
     {
-        $books = BookirBook::where('xparent', '=', '0')->take(500)->get();
+        $books = BookirBook::where('xparent', '=', '0')->take(200)->get();
         if($books != null)
         {
             foreach ($books as $book)
