88f0956850d5b6426141fc6a1e50b7700f41ad69
mohan21 <firm_mohan21@yahoo.com>
2022-02-11 16:09:14 +0330

    report :: add Publisher

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -19,21 +19,13 @@ class ReportController extends Controller
         $publisherId = (isset($request["publisherId"])) ? $request["publisherId"] : 0;
         $yearStart = (isset($request["yearStart"])) ? $request["yearStart"] : 0;
         $yearEnd = (isset($request["yearEnd"])) ? $request["yearEnd"] : 0;
-        $currentPageNumber = (isset($request["currentPageNumber"])) ? $request["currentPageNumber"] : 0;
         $data = null;
         $status = 404;
-        $pageRows = 50;
-        $totalRows = 0;
-        $totalPages = 0;
-        $offset = ($currentPageNumber - 1) * $pageRows;
 
         // read
         $books = BookirBook::orderBy('xpublishdate', 'desc');
         $books->whereRaw("xid In (Select bi_book_xid From bi_book_bi_publisher Where bi_publisher_xid='$publisherId')");
-
-        $totalRows = $books->count(); // get count records
-        $books = $books->skip($offset)->take($pageRows)->get(); // get list
-
+        $books = $books->get(); // get list
         if($books != null and count($books) > 0)
         {
             foreach ($books as $book)
@@ -68,16 +60,13 @@ class ReportController extends Controller
             $status = 200;
         }
 
-        //
-        $totalPages = $totalRows > 0 ? (int) ceil($totalRows / $pageRows) : 0;
-
         // response
         return response()->json
         (
             [
                 "status" => $status,
                 "message" => $status == 200 ? "ok" : "not found",
-                "data" => ["list" => $data, "currentPageNumber" => $currentPageNumber, "totalPages" => $totalPages, "pageRows" => $pageRows, "totalRows" => $totalRows]
+                "data" => ["list" => $data]
             ],
             $status
         );
