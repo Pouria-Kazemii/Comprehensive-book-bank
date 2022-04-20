27b91bfb9cae6cb2562554124f32aa22f6f870a6
Kiani <dpmdeveloper1@gmail.com>
2022-04-20 16:57:52 +0430

    test consensus_similar_books function

--- app/Http/Controllers/ChangeDataController.php
+++ app/Http/Controllers/ChangeDataController.php
@@ -240,20 +240,20 @@ class ChangeDataController extends Controller
                     $strongBookIsbn  = $bookirBookParentItem;
                     $strongBookId  = $key;
                 }
-                echo 'id : ' . $strongBookId . 'isbn : ' . $strongBookIsbn . 'count : ' . $strongBookCount;
+                echo 'id : ' . $strongBookId . 'isbn : ' . $strongBookIsbn . 'count : ' . $strongBookCount.'</br>';
 
             }
             
-            // DB::enableQueryLog();
-            // try {
-            //     BookirBook::whereIN('xid', $allBookirBooksIdCollection)->update(['xtempparent' => $strongBookId]);
-            //     BookirBook::where('xid', $strongBookId)->update(['xtempparent' => -1]);
-            //     echo 'id : ' . $strongBookId . 'isbn : ' . $strongBookIsbn . 'count : ' . $strongBookCount;
+            DB::enableQueryLog();
+            try {
+                BookirBook::whereIN('xid', $allBookirBooksIdCollection)->update(['xtempparent' => $strongBookId]);
+                BookirBook::where('xid', $strongBookId)->update(['xtempparent' => -1]);
+                echo 'id : ' . $strongBookId . 'isbn : ' . $strongBookIsbn . 'count : ' . $strongBookCount;
 
-            // } catch (Exception $Exception) {
-            //     //throw $th;
-            //    echo " update bookirbook temp_book_master_id exception error " . $Exception->getMessage();
-            // }
+            } catch (Exception $Exception) {
+                //throw $th;
+               echo " update bookirbook temp_book_master_id exception error " . $Exception->getMessage();
+            }
            
         }
     }
