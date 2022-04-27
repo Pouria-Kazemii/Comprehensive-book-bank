6438815d498bc767e38d3e4b3b6fe5be1b2fd4ad
Kiani <dpmdeveloper1@gmail.com>
2022-04-27 14:24:00 +0430

    update in merge_parentid_tempparentid

--- app/Http/Controllers/ChangeDataController.php
+++ app/Http/Controllers/ChangeDataController.php
@@ -339,7 +339,8 @@ class ChangeDataController extends Controller
                 // echo ' book_id : ' . $bookItem->id . ' book parentId : ' . $bookItem->parentId  .' book xtempparent : ' . $bookItem->xtempparent  . '</br>';
                 foreach($all_old_parents as $all_old_parent_item){
                     echo $all_old_parent_item.'</br>';
-                    BookirBook::where('xtempparent', 0)->where('xparent',$all_old_parent_item)->update(['xtempparent' => $all_old_parent_item]);
+                    // BookirBook::where('xtempparent', 0)->where('xparent',$all_old_parent_item)->update(['xtempparent' => $bookItem->xtempparent]);
+                    BookirBook::where('xparent',$all_old_parent_item)->update(['xtempparent' => $bookItem->xtempparent]);
                 }
 
             }
