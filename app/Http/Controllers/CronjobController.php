f18de658125434e1c9b7d07d16974ae9dafbb621
Kiani <dpmdeveloper1@gmail.com>
2022-09-04 14:22:02 +0430

    add fill_circulation_temp_table cronjob

--- app/Http/Controllers/CronjobController.php
+++ app/Http/Controllers/CronjobController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BookirPublisher;
+use App\Models\BookirPartner;
 use App\Models\BookirBook;
+use App\Models\CirculationTemp;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -66,9 +69,165 @@ class CronjobController extends Controller
                     BookirBook::where('xid', $book->xid)->update($updateData);
                 }
             }
-        }); 
+        });
 
         echo '</br>';
         echo 'end : ' . date("Y/m/d H:i:s");
+    }
+    public function fill_circulation_temp_table(){
+        echo 'start : ' . date("Y/m/d H:i:s");
+        $books = BookirBook::where('check_circulation' , 0)->limit(1)->get();
+        if(isset($books) AND !empty($books)){
+            foreach($books as $book){
+                $this->check_book_circulation($book->xid);
+                $book->check_circulation = 1;
+                $book->update();
+            }
+        }
+        echo 'end : ' . date("Y/m/d H:i:s");
+
+    }
+
+    public function check_book_circulation($book_id)
+    {
+        // DB::enableQueryLog();
+        $book_info = BookirBook::where('xid',$book_id)->first();
+        if($book_info->xparent == -1 ){
+            $searchBookId = $book_info->xid;
+        }else{
+            $searchBookId = $book_info->xparent;
+        }
+        $books =  BookirBook::with('publishers', 'partnersRoles', 'children')->where('xid',$searchBookId)->get();
+        // $query = DB::getQueryLog();
+        // echo '<pre>'; print_r($query);
+        // dd($query);
+        // dd($books);
+        // die('stop');
+        if (isset($books) and !empty($books)) {
+            foreach ($books as $book) {
+                // echo '$book->xid : '.$book->xid.'</br>';
+                //reset book circulation year record
+                CirculationTemp::where('xbook_id', $book->xid)->delete();
+                $circulationTempModel = new CirculationTemp([
+                    'xbook_id' => $book->xid,
+                    'xcirculations_count' => $book->xcirculation,
+                    'xfirst_edition_circulations_count' => ($book->xprintnumber == 1) ? $book->xcirculation : 0,
+                    'xyear' => BookirBook::getShamsiYear($book->xpublishdate),
+                ]);
+                $circulationTempModel->save();
+                // children 
+                if ($book->children()->exists()) {
+                    // dd($book->children);
+                    // echo '$book->xcirculation  : '. $book->xid . '******' .BookirBook::getShamsiYear($book->xpublishdate) .'=>'.$book->xcirculation.'</br>';
+                    foreach ($book->children as $book_children) {
+                        // echo 'book_children->xcirculation : '. $book_children->xid . '******'.BookirBook::getShamsiYear($book_children->xpublishdate) .'=>'.$book_children->xcirculation.'</br>';
+                        $selectedCirculationTempInfo = CirculationTemp::where('xbook_id', $book->xid)->where('xyear', BookirBook::getShamsiYear($book_children->xpublishdate))->first();
+                        if (isset($selectedCirculationTempInfo) and !empty($selectedCirculationTempInfo)) {
+                            $selectedCirculationTempInfo->xcirculations_count =  $selectedCirculationTempInfo->xcirculations_count + $book_children->xcirculation;
+                            if ($book_children->xprintnumber == 1) {
+                                $selectedCirculationTempInfo->xfirst_edition_circulations_count = $selectedCirculationTempInfo->xfirst_edition_circulations_count + $book_children->xcirculation;
+                            }
+                            $selectedCirculationTempInfo->update();
+                        } else {
+                            $circulationTempModel = new CirculationTemp([
+                                'xbook_id' => $book->xid,
+                                'xcirculations_count' => $book_children->xcirculation,
+                                'xfirst_edition_circulations_count' => ($book_children->xprintnumber == 1) ? $book_children->xcirculation : 0,
+                                'xyear' => BookirBook::getShamsiYear($book_children->xpublishdate),
+                            ]);
+                            $circulationTempModel->save();
+                        }
+                    }
+                }
+
+                // publisher
+                if ($book->publishers()->exists()) {
+                    // dd($book->publishers);
+                    foreach ($book->publishers as $book_publishers) {
+                        // echo $book_publishers->xid;
+                        // echo '<br>';
+                        $books_of_book_publishers = BookirPublisher::with('books')->where('xid', $book_publishers->xid)->get(); // کتاب های ناشران کتاب
+                        if (isset($books_of_book_publishers) and !empty($books_of_book_publishers)) {
+                            foreach ($books_of_book_publishers as $books_of_book_publisher) { // کتاب های ناشر کتاب
+                                if ($books_of_book_publisher->books()->exists()) {
+                                    CirculationTemp::where('xpublisher_id', $books_of_book_publisher->xid)->delete();
+                                    // dd($books_of_book_publisher);
+                                    // dd($books_of_book_publisher->books);
+                                    foreach($books_of_book_publisher->books as $publisher_books){
+                                        $selectedCirculationTempInfo = CirculationTemp::where('xpublisher_id', $books_of_book_publisher->xid)->where('xyear', BookirBook::getShamsiYear($publisher_books->xpublishdate))->first();
+                                        if (isset($selectedCirculationTempInfo) and !empty($selectedCirculationTempInfo)) {
+                                            $selectedCirculationTempInfo->xbooks_count =  $selectedCirculationTempInfo->xbooks_count +1;
+                                            if ($publisher_books->xprintnumber == 1) {
+                                                $selectedCirculationTempInfo->xfirst_edition_books_count = $selectedCirculationTempInfo->xfirst_edition_books_count + 1;
+                                            }
+                                            $selectedCirculationTempInfo->xcirculations_count =  $selectedCirculationTempInfo->xcirculations_count + $publisher_books->xcirculation;
+                                            if ($publisher_books->xprintnumber == 1) {
+                                                $selectedCirculationTempInfo->xfirst_edition_circulations_count = $selectedCirculationTempInfo->xfirst_edition_circulations_count + $publisher_books->xcirculation;
+                                            }
+                                            $selectedCirculationTempInfo->update();
+                                        } else {
+                                            $circulationTempModel = new CirculationTemp([
+                                                'xpublisher_id' => $books_of_book_publisher->xid,
+                                                'xbooks_count' => 1,
+                                                'xfirst_edition_books_count' => ($publisher_books->xprintnumber == 1) ? 1 : 0,
+                                                'xcirculations_count' => $publisher_books->xcirculation,
+                                                'xfirst_edition_circulations_count' => ($publisher_books->xprintnumber == 1) ? $publisher_books->xcirculation : 0,
+                                                'xyear' => BookirBook::getShamsiYear($publisher_books->xpublishdate),
+                                            ]);
+                                            $circulationTempModel->save();
+                                        }
+                                    }
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+
+                // author
+                if ($book->partnersRoles()->exists()) {
+                    //  dd($book->partnersRoles);
+                    foreach ($book->partnersRoles as $book_authors) {
+                        // echo $book_authors->xid;
+                        // echo '<br>';
+                        $books_of_book_authors = BookirPartner::with('books')->where('xid', $book_authors->xid)->get(); // کتاب های پدیدآورندگان کتاب
+                        if (isset($books_of_book_authors) and !empty($books_of_book_authors)) {
+                            foreach ($books_of_book_authors as $books_of_book_author) { // کتاب های پدیدآورنده کتاب
+                                if ($books_of_book_author->books()->exists()) {
+                                    CirculationTemp::where('xauthor_id', $books_of_book_author->xid)->delete();
+                                    // dd($books_of_book_publisher);
+                                    // dd($books_of_book_author->books);
+                                    foreach($books_of_book_author->books as $author_books){
+                                        $selectedCirculationTempInfo = CirculationTemp::where('xauthor_id', $books_of_book_author->xid)->where('xyear', BookirBook::getShamsiYear($author_books->xpublishdate))->first();
+                                        if (isset($selectedCirculationTempInfo) and !empty($selectedCirculationTempInfo)) {
+                                            $selectedCirculationTempInfo->xbooks_count =  $selectedCirculationTempInfo->xbooks_count +1;
+                                            if ($author_books->xprintnumber == 1) {
+                                                $selectedCirculationTempInfo->xfirst_edition_books_count = $selectedCirculationTempInfo->xfirst_edition_books_count + 1;
+                                            }
+                                            $selectedCirculationTempInfo->xcirculations_count =  $selectedCirculationTempInfo->xcirculations_count + $author_books->xcirculation;
+                                            if ($author_books->xprintnumber == 1) {
+                                                $selectedCirculationTempInfo->xfirst_edition_circulations_count = $selectedCirculationTempInfo->xfirst_edition_circulations_count + $author_books->xcirculation;
+                                            }
+                                            $selectedCirculationTempInfo->update();
+                                        } else {
+                                            $circulationTempModel = new CirculationTemp([
+                                                'xauthor_id' => $books_of_book_author->xid,
+                                                'xbooks_count' => 1,
+                                                'xfirst_edition_books_count' => ($author_books->xprintnumber == 1) ? 1 : 0,
+                                                'xcirculations_count' => $author_books->xcirculation,
+                                                'xfirst_edition_circulations_count' => ($author_books->xprintnumber == 1) ? $author_books->xcirculation : 0,
+                                                'xyear' => BookirBook::getShamsiYear($author_books->xpublishdate),
+                                            ]);
+                                            $circulationTempModel->save();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
     }
 }
