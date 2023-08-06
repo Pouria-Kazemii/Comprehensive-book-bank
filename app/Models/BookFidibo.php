2207e45d123aa74967f31056ed0cadc3f2ce44d6
dpmdeveloper1@gmail.com <dmpdeveloper1@gmail.com>
2023-08-06 05:39:48 -0400

    change book fidibo to bookFidibo1

--- app/Models/BookFidibo.php
+++ app/Models/BookFidibo.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class BookFidibo extends Model
 {
     protected $fillable = ['recordNumber','parentId','title','nasher','saleNashr', 'tedadSafe', 'shabak','translate','desc','images','price','lang', 'fileSize', 'partnerArray','tags'];
-    protected $table = 'bookFidibo';
+    protected $table = 'bookFidibo1';
     protected $primaryKey = 'id';
 
     // public function authors() {
