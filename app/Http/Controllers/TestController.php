5c1a4123c35a3bf35c504d3b0980ed84b17e8735
Kiani <dpmdeveloper1@gmail.com>
2023-08-27 11:11:54 +0430

    add web route for test majma api

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BookirPublisher;
+use App\Models\BookirPartner;
+use App\Models\BookirBook;
+use App\Models\CirculationTemp;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Exception;
+
+class TestController extends Controller
+{
+    public function test_majma_api(){
+        $timeout = 120;
+        $url = 'https://core.ketab.ir/api/Majma/get-books/?MaxResultCount=200&SkipCount=0&From=2023-08-18&To=2023-08-20';
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_ENCODING, "");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+        curl_setopt($ch, CURLOPT_REFERER, 'http://www.google.com/');
+        $content = curl_exec($ch);
+        var_dump($content);
+    }
+}
