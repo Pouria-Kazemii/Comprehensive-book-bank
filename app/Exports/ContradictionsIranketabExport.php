061a5ae5a4c67db83c250c5647da50f72f7612a2
unknown <dpmdeveloper1@gmail.com>
2023-09-27 11:03:46 +0330

    fixed error iranketab export excel

--- app/Exports/ContradictionsIranketabExport.php
+++ app/Exports/ContradictionsIranketabExport.php
@@ -19,7 +19,7 @@ class ContradictionsIranketabExport implements FromCollection,WithHeadings
         $data = array();
         // DB::enableQueryLog();
         DB::statement("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
-        $report = BookIranketab::select('recordNumber','title','nasher','saleNashr','tedadSafe','shabak','traslate','price','check_status','has_permit','images')->where('title','!=',NULL)->whereIN('has_permit',  $status)->whereIN('check_status',$status)->get();
+        $report = BookIranketab::select('recordNumber','title','nasher','saleNashr','tedadSafe','shabak','traslate','price','check_status','tags','has_permit','images')->where('title','!=',NULL)->whereIN('has_permit',  $status)->whereIN('check_status',$status)->get();
         foreach($report as $key=>$item){
             if($item->traslate == 1 ){
                 $report[$key]->traslate = 'ترجمه';
@@ -27,36 +27,44 @@ class ContradictionsIranketabExport implements FromCollection,WithHeadings
                 $report[$key]->traslate = 'تالیف';
             }
 
+            $report[$key]->tags = '';
+            if($item->check_status == 2){
+                if((isset($item->saleNashr) and $item->saleNashr != null and !empty($item->saleNashr))){
+                    $georgianCarbonDate=\Morilog\Jalali\Jalalian::fromFormat('Y/m/d', $item->saleNashr)->toCarbon();
+                    if(strtotime($georgianCarbonDate) > strtotime('2022-03-21 00:00:00')){
+                        $report[$key]->tags = '*';
+                    }
+                }
+            }
             if($item->check_status == 1){
-                $report[$key]->check_status = 'کتاب وجود دارد';
+                $report[$key]->check_status = 'کتاب در خانه کتاب وجود دارد';
             }elseif($item->check_status == 2){
-                $report[$key]->check_status = 'کتاب وجود ندارد';
+                $report[$key]->check_status = 'کتاب در خانه کتاب وجود ندارد';
             }elseif($item->check_status == 3){
                 $report[$key]->check_status = 'جستجو نشده به دلیل محدودیت سال انتشار';
             }elseif($item->check_status == 4){
                 $report[$key]->check_status = 'کتاب شابک ندارد';
             }
-          
+
+            
+            $report[$key]->images = '';
+            if($item->has_permit == 2){
+                if((isset($item->saleNashr) and $item->saleNashr != null and !empty($item->saleNashr))){
+                    $georgianCarbonDate=\Morilog\Jalali\Jalalian::fromFormat('Y/m/d', $item->saleNashr)->toCarbon();
+                    if(strtotime($georgianCarbonDate) < strtotime('2018-03-21 00:00:00')){
+                        $report[$key]->images = '**';
+                    }
+                }
+            }
            
             if($item->has_permit == 1){
-                $report[$key]->has_permit = 'کتاب وجود دارد';
+                $report[$key]->has_permit = 'کتاب در اداره کتاب وجود دارد';
             }elseif($item->has_permit == 2){
-                $report[$key]->has_permit = 'کتاب وجود ندارد';
+                $report[$key]->has_permit = 'کتاب در اداره کتاب وجود ندارد';
             }elseif($item->has_permit == 3){
                 $report[$key]->has_permit = 'جستجو نشده به دلیل محدودیت سال انتشار';
             }elseif($item->has_permit == 4){
                 $report[$key]->has_permit = 'کتاب شابک ندارد';
-            }
-
-            if(($item->has_permit == 2 OR $item->check_status == 2) and (isset($report[$key]->saleNashr) and $report[$key]->saleNashr != null and !empty($report[$key]->saleNashr))){
-                $georgianCarbonDate=\Morilog\Jalali\Jalalian::fromFormat('Y/m/d', $report[$key]->saleNashr)->toCarbon();
-                if ($georgianCarbonDate < date('2022-03-21 00:00:00') OR $georgianCarbonDate > date('2018-03-21 00:00:00') ) {
-                    $report[$key]->images = '('.$item->saleNashr.' )جستجو نشده به دلیل محدودیت سال انتشار';
-                }else{
-                    $report[$key]->images = '';
-                }
-            }else{
-                $report[$key]->images = '';
             }
 
             
@@ -67,6 +75,6 @@ class ContradictionsIranketabExport implements FromCollection,WithHeadings
 
     public function headings(): array
     {
-        return ["لینک کتاب در ایران کتاب", "عنوان کتاب","ناشر","تاریخ انتشار","تعداد صفحه","شابک","تالیف یا ترجمه","قیمت","وضعیت در خانه کتاب","وضعیت در اداره کتاب","راهنما"];
+        return ["لینک کتاب در ایران کتاب", "عنوان کتاب","ناشر","تاریخ انتشار","تعداد صفحه","شابک","تالیف یا ترجمه","قیمت","وضعیت در خانه کتاب","راهنمای خانه کتاب","وضعیت در اداره کتاب","راهنمای اداره کتاب"];
     }
 }
