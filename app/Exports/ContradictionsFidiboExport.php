2b176193f25bf0b262aa469f44de544e96ea375c
unknown <dpmdeveloper1@gmail.com>
2023-09-27 12:02:38 +0330

    change fidibo export contraditional list

--- app/Exports/ContradictionsFidiboExport.php
+++ app/Exports/ContradictionsFidiboExport.php
@@ -20,7 +20,7 @@ class ContradictionsFidiboExport implements FromCollection,WithHeadings
         $data = array();
         // DB::enableQueryLog();
         DB::statement("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
-        $report = BookFidibo::select('recordNumber','title','nasher','saleNashr','tedadSafe','shabak','translate','lang','fileSize')->where('saleNashr','<','1400/01/01')->where('title','!=',NULL)->where('has_permit',  $status)->get();
+        $report = BookFidibo::select('recordNumber','title','nasher','saleNashr','tedadSafe','shabak','translate','lang','fileSize','check_status','tags','has_permit','images')->where('title','!=',NULL)->whereIN('has_permit',  $status)->whereIN('check_status',$status)->get();
         foreach($report as $key=>$item){
             if($item->translate == 1 ){
                 $report[$key]->translate = 'ترجمه';
@@ -32,6 +32,47 @@ class ContradictionsFidiboExport implements FromCollection,WithHeadings
             }else{
                 $report[$key]->fileSize = 'چاپی';
             }
+
+            $report[$key]->tags = '';
+            if($item->check_status == 2){
+                if((isset($item->saleNashr) and $item->saleNashr != null and !empty($item->saleNashr))){
+                    $georgianCarbonDate=\Morilog\Jalali\Jalalian::fromFormat('Y/m/d', $item->saleNashr)->toCarbon();
+                    if(strtotime($georgianCarbonDate) > strtotime('2022-03-21 00:00:00')){
+                        $report[$key]->tags = '*';
+                    }
+                }
+            }
+            if($item->check_status == 1){
+                $report[$key]->check_status = 'کتاب در خانه کتاب وجود دارد';
+            }elseif($item->check_status == 2){
+                $report[$key]->check_status = 'کتاب در خانه کتاب وجود ندارد';
+            }elseif($item->check_status == 3){
+                $report[$key]->check_status = 'جستجو نشده به دلیل محدودیت سال انتشار';
+            }elseif($item->check_status == 4){
+                $report[$key]->check_status = 'کتاب شابک ندارد';
+            }
+
+            
+            $report[$key]->images = '';
+            if($item->has_permit == 2){
+                if((isset($item->saleNashr) and $item->saleNashr != null and !empty($item->saleNashr))){
+                    $georgianCarbonDate=\Morilog\Jalali\Jalalian::fromFormat('Y/m/d', $item->saleNashr)->toCarbon();
+                    if(strtotime($georgianCarbonDate) < strtotime('2018-03-21 00:00:00')){
+                        $report[$key]->images = '**';
+                    }
+                }
+            }
+           
+            if($item->has_permit == 1){
+                $report[$key]->has_permit = 'کتاب در اداره کتاب وجود دارد';
+            }elseif($item->has_permit == 2){
+                $report[$key]->has_permit = 'کتاب در اداره کتاب وجود ندارد';
+            }elseif($item->has_permit == 3){
+                $report[$key]->has_permit = 'جستجو نشده به دلیل محدودیت سال انتشار';
+            }elseif($item->has_permit == 4){
+                $report[$key]->has_permit = 'کتاب شابک ندارد';
+            }
+
             $report[$key]->recordNumber = 'https://fidibo.com/book/'.$item->recordNumber;
 
         }
@@ -40,6 +81,6 @@ class ContradictionsFidiboExport implements FromCollection,WithHeadings
 
     public function headings(): array
     {
-        return ["لینک کتاب در فیدیبو", "عنوان کتاب","ناشر","تاریخ انتشار","تعداد صفحه","شابک","تالیف یا ترجمه","زبان","چاپی یا الکترونیکی"];
+        return ["لینک کتاب در فیدیبو", "عنوان کتاب","ناشر","تاریخ انتشار","تعداد صفحه","شابک","تالیف یا ترجمه","زبان","چاپی یا الکترونیکی","وضعیت در خانه کتاب","راهنمای خانه کتاب","وضعیت در اداره کتاب","راهنمای اداره کتاب"];
     }
 }
