7e3ae1a4aeadcc87a0e84afe620884c3b2307bf7
unknown <dpmdeveloper1@gmail.com>
2023-11-04 13:05:10 +0330

    change kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('get:iranKetab 1')->everyMinute();
-        $schedule->command('get:GetMajmaForCorrectInfo 1')->everySecond()->timezone('Asia/Tehran')->between('02:00', '6:00');
+        $schedule->command('get:GetMajmaForCorrectInfo 1')->everyMinute()->timezone('Asia/Tehran')->between('02:00', '6:00');
     }
 
     /**
