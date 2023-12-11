31fed6f086192f5eb44b0721de8708636c812fb3
unknown <dpmdeveloper1@gmail.com>
2023-12-11 10:11:28 +0330

    add check_status and has_permit for old and new info

--- app/Models/WebSiteBookLinksDefects.php
+++ app/Models/WebSiteBookLinksDefects.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class WebSiteBookLinksDefects extends Model
 {
-    protected $fillable = ['id', 'siteName', 'recordNumber','book_links', 'bookId','bugId','crawlerInfo','crawlerStatus','crawlerTime','result','excelId'];
+    protected $fillable = ['id', 'siteName', 'recordNumber','book_links', 'bookId','bugId','old_check_status','old_has_permit','crawlerInfo','crawlerStatus','crawlerTime','new_check_status','new_has_permit','result','excelId'];
     protected $table = 'tbl_website_book_links_defects';
     protected $primaryKey = 'id';
     public $timestamps = true;
