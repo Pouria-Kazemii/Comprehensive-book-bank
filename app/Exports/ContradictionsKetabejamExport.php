d60ef034cd302c6d2ae425e9cf00e6a875f6f36e
unknown <dpmdeveloper1@gmail.com>
2023-12-25 16:51:13 +0330

    change ketabejam export

--- app/Exports/ContradictionsKetabejamExport.php
+++ app/Exports/ContradictionsKetabejamExport.php
@@ -23,11 +23,11 @@ class ContradictionsKetabejamExport implements FromCollection,WithHeadings
         $data = array();
         // DB::enableQueryLog();
         DB::statement("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
-        $report = BookKetabejam::select('pageUrl','title','nasher','tedadSafe','shabak','ghatechap','check_status','cats','has_permit','images')->where('title','!=',NULL)->whereIN('has_permit',  $status)->whereIN('check_status',$status)->get();
+        $report = BookKetabejam::select('pageUrl','title','nasher','tedadSafe','saleNashr','shabak','ghatechap','check_status','cats','has_permit','images')->where('title','!=',NULL)->whereIN('has_permit',  $status)->whereIN('check_status',$status)->get();
         foreach($report as $key=>$item){
             $report[$key]->cats = '';
             if($item->check_status == 2){
-                if((isset($item->saleNashr) and $item->saleNashr != null and !empty($item->saleNashr))){
+                if((isset($item->saleNashr) and $item->saleNashr != NULL and !empty($item->saleNashr))){
                     $georgianCarbonDate=\Morilog\Jalali\Jalalian::fromFormat('Y/m/d', substr($item->saleNashr,0,4).'/01/01')->toCarbon();
                     if(strtotime($georgianCarbonDate) > strtotime('2022-03-21 00:00:00')){
                         $report[$key]->cats = '*';
@@ -38,7 +38,7 @@ class ContradictionsKetabejamExport implements FromCollection,WithHeadings
            
             $report[$key]->images = '';
             if($item->has_permit == 2){
-                if((isset($item->saleNashr) and $item->saleNashr != null and !empty($item->saleNashr))){
+                if((isset($item->saleNashr) and $item->saleNashr != NULL and !empty($item->saleNashr))){
                     $georgianCarbonDate=\Morilog\Jalali\Jalalian::fromFormat('Y/m/d', substr($item->saleNashr,0,4).'/01/01')->toCarbon();
                     if(strtotime($georgianCarbonDate) < strtotime('2018-03-21 00:00:00')){
                         $report[$key]->images = '**';
@@ -57,6 +57,6 @@ class ContradictionsKetabejamExport implements FromCollection,WithHeadings
 
     public function headings(): array
     {
-        return ["آیدی کتاب در کتاب جم", "عنوان کتاب","ناشر","تعداد صفحه","شابک","قطع","وضعیت در خانه کتاب","راهنمای خانه کتاب","وضعیت در اداره کتاب","راهنمای اداره کتاب"];
+        return ["آیدی کتاب در کتاب جم", "عنوان کتاب","ناشر","تعداد صفحه","سال نشر","شابک","قطع","وضعیت در خانه کتاب","راهنمای خانه کتاب","وضعیت در اداره کتاب","راهنمای اداره کتاب"];
     }
 }
