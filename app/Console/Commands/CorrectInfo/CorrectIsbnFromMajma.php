032e5902887244861602292a78d0ebdf026c1488
dpmdeveloper1@gmail.com <dmpdeveloper1@gmail.com>
2024-02-15 09:05:50 -0400

    change cammand and function for give api majma fr old records

--- app/Console/Commands/CorrectInfo/CorrectIsbnFromMajma.php
+++ app/Console/Commands/CorrectInfo/CorrectIsbnFromMajma.php
@@ -2,21 +2,11 @@
 
 namespace App\Console\Commands\CorrectInfo;
 
-use App\Models\AgeGroup;
-use App\Models\BookCover;
-use App\Models\BookFormat;
 use App\Models\BookirBook;
-use App\Models\BookirPartner;
-use App\Models\BookirPublisher;
-use App\Models\BookirRules;
-use App\Models\BookirSubject;
-use App\Models\BookLanguage;
 use App\Models\Crawler as CrawlerM;
-use App\Models\MajmaApiBook;
-use App\Models\MajmaApiPublisher;
-use Goutte\Client;
 use Illuminate\Console\Command;
-use Symfony\Component\HttpClient\HttpClient;
+
+
 
 class CorrectIsbnFromMajma extends Command
 {
@@ -52,7 +42,7 @@ class CorrectIsbnFromMajma extends Command
     public function handle()
     {
         $function_caller = 'Correct-Isbn-From-Majma';
-        $total = BookirBook::whereNotNull('xpageurl2')->where('xisbn', 'not like', "%-%")->where('check_circulation', 0)->where('xid', '<', 500000)->count();
+        $total = BookirBook::where('xisbn', 'not like', "%-%")->where('check_circulation', 0)->where('xid', '<', 500000)->count();
         try {
             $startC = 0;
             $endC   = $total;
@@ -69,22 +59,26 @@ class CorrectIsbnFromMajma extends Command
             $bar->start();
 
             // BookirBook::whereNotNull('xpageurl2')->where('xisbn', 'not like', "%-%")->where('check_circulation', 0)->where('xid', '<', 500000)->orderby('xid', 'ASC')->chunk(2000, function ($books) use ($bar, $function_caller, $newCrawler) {
-            $books = BookirBook::whereNotNull('xpageurl2')->where('xisbn', 'not like', "%-%")->where('check_circulation', 0)->where('xid', '<', 500000)->orderby('xid', 'ASC')->limit(60)->get();
+            $books = BookirBook::where('xisbn', 'not like', "%-%")->where('check_circulation', 0)->where('xid', '<', 500000)->orderby('xid', 'ASC')->limit(60)->get();
             foreach ($books as $book) {
 
                 $pageUrl = str_replace("http://ketab.ir/bookview.aspx?bookid=", '', $book->xpageurl);
                 $recordNumber = str_replace("https://db.ketab.ir/bookview.aspx?bookid=", '', $pageUrl);
                 $this->info('recordNumber :' . $recordNumber);
 
-                $apiResult = returnBookDataFromMajmaApi($recordNumber, $function_caller);
-                if ($apiResult) {
-                    $book_content = $apiResult;
-                    $book_content->isbn = validateIsbn($book_content->isbn);
-                    $this->info($book_content->isbn);
-                    $book->xisbn = (!is_null($book_content->isbn) && !empty($book_content->isbn)) ? $book_content->isbn : $book->xisbn;
-                    $book->save();
-                }
-                BookirBook::where('xid', $book->xid)->update(['check_circulation' => 1]);
+                $bookIrBook = BookirBook::where('xid', $book->xid)->first();
+                $api_status = updateBookDataWithMajmaApiInfo($recordNumber, $bookIrBook, $function_caller);
+                $bookIrBook->check_circulation = $api_status;
+                $bookIrBook->save();
+                
+                // $apiResult = returnBookDataFromMajmaApi($recordNumber, $function_caller);
+                // if ($apiResult) {
+                //     $book_content = $apiResult;
+                //     $book_content->isbn = validateIsbn($book_content->isbn);
+                //     $this->info($book_content->isbn);
+                //     $book->xisbn = (!is_null($book_content->isbn) && !empty($book_content->isbn)) ? $book_content->isbn : $book->xisbn;
+                //     $book->save();
+                // }
 
                 $bar->advance();
                 $newCrawler->last = $recordNumber;
--- app/Console/Commands/CorrectInfo/CorrectIsbnFromMajma4.php
+++ app/Console/Commands/CorrectInfo/CorrectIsbnFromMajma4.php
@@ -2,18 +2,19 @@
 
 namespace App\Console\Commands\CorrectInfo;
 
+
 use App\Models\BookirBook;
 use App\Models\Crawler as CrawlerM;
 use Illuminate\Console\Command;
 
-class CorrectIsbnFromMajma2 extends Command
+class CorrectIsbnFromMajma4 extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'get:CorrectIsbnFromMajma2 {crawlerId}';
+    protected $signature = 'get:CorrectIsbnFromMajma4 {crawlerId} ';
 
     /**
      * The console command description.
@@ -39,28 +40,28 @@ class CorrectIsbnFromMajma2 extends Command
      */
     public function handle()
     {
-        $function_caller = 'Correct-Isbn-From-Majma2';
-        $total = BookirBook::where('xisbn', 'not like', "%-%")->where('check_circulation', 0)->where('xid', '>', 1000000)->where('xid', '<', 1500000)->count();
+        $function_caller = 'Correct-Isbn-From-Majma4';
+        $total = BookirBook::where('xisbn', 'not like', "%-%")->where('check_circulation', 0)->where('xid', '>', 2000000)->count();
         try {
             $startC = 0;
             $endC   = $total;
             $this->info(" \n ---------- Create Crawler  " . $this->argument('crawlerId') . "     $startC  -> $endC         ---------=-- ");
-            $newCrawler = CrawlerM::firstOrCreate(array('name' => 'Correct-Isbn-From-Majma2-' . $this->argument('crawlerId'), 'start' => $startC, 'end' => $endC, 'status' => 1, 'type' => 2));
+            $newCrawler = CrawlerM::firstOrCreate(array('name' => 'Correct-Isbn-From-Majma4-' . $this->argument('crawlerId'), 'start' => $startC, 'end' => $endC, 'status' => 1, 'type' => 2));
         } catch (\Exception $e) {
+
             $this->info(" \n ---------- Failed Crawler  " . $this->argument('crawlerId') . "              ---------=-- ");
         }
 
+
+
         if (isset($newCrawler)) {
-
-
             $bar = $this->output->createProgressBar($total);
             $bar->start();
 
-            // BookirBook::whereNotNull('xpageurl2')->where('xisbn', 'not like', "%-%")->where('check_circulation', 0)->where('xid', '>', 1000000)->where('xid', '<', 1500000)->orderby('xid', 'ASC')->chunk(2000,  function ($books) use ($bar, $function_caller, $newCrawler) {
-            $books = BookirBook::where('xisbn', 'not like', "%-%")->where('check_circulation', 0)->where('xid', '>', 1000000)->where('xid', '<', 1500000)->orderby('xid', 'ASC')->limit(60)->get();
-
+            // BookirBook::whereNotNull('xpageurl2')->where('xisbn', 'not like', "%-%")->where('check_circulation', 0)->where('xid', '>', 2000000)->orderby('xid', 'ASC')->chunk(2000, function ($books) use ($bar, $function_caller, $newCrawler) {
+            $books = BookirBook::where('xisbn', 'not like', "%-%")->where('check_circulation', 0)->where('xid', '>', 2000000)->orderby('xid', 'ASC')->limit(60)->get();
             foreach ($books as $book) {
-                BookirBook::where('xid', $book->xid)->update(['check_circulation' => 1]);
+                $this->info($book->xid);
 
                 $pageUrl = str_replace("http://ketab.ir/bookview.aspx?bookid=", '', $book->xpageurl);
                 $recordNumber = str_replace("https://db.ketab.ir/bookview.aspx?bookid=", '', $pageUrl);
@@ -79,6 +80,7 @@ class CorrectIsbnFromMajma2 extends Command
                 //     $book->xisbn = (!is_null($book_content->isbn) && !empty($book_content->isbn)) ? $book_content->isbn : $book->xisbn;
                 //     $book->save();
                 // }
+                // BookirBook::where('xid', $book->xid)->update(['check_circulation' => 1]);
 
                 $bar->advance();
                 $newCrawler->last = $recordNumber;
