dd9cd37640bda670bbb7fba41cc92b6d5af86e75
dpmdeveloper1@gmail.com <dmpdeveloper1@gmail.com>
2024-04-23 04:54:27 -0400

    change book digi contradicyion list command

--- app/Console/Commands/ContradictionsList/GetDigiContradictionsList.php
+++ app/Console/Commands/ContradictionsList/GetDigiContradictionsList.php
@@ -41,47 +41,20 @@ class GetDigiContradictionsList extends Command
      */
     public function handle()
     {
-        if ($this->argument('miss') && $this->argument('miss') == 1) {
-            try {
-                $lastCrawler = CrawlerM::where('name', 'Contradictions-digi-' . $this->argument('rowId'))->where('status', 1)->orderBy('id', 'DESC')->first();
-                if (isset($lastCrawler) AND !empty($lastCrawler)) {
-                    $startC = $lastCrawler->last;
-                    $endC   = $lastCrawler->end;
-                    $this->info(" \n ---------- Check  " . $this->argument('rowId') . "     $startC  -> $endC         ------------ ");
-                    $newCrawler = CrawlerM::firstOrCreate(array('name' => 'Contradictions-digi-' . $this->argument('crawlerId'), 'start' => $startC, 'end' => $endC, 'status' => 1, 'type' => 2));
+        $check_count = BookDigi::where('check_status', 0)->where('has_permit', 0)->count();
 
-                }
-            } catch (\Exception $e) {
-                $this->info(" \n ---------- Failed Crawler  " . $this->argument('rowId') . "              ------------ ");
-            }
-        } else {
-            try {
-                $lastCrawler = CrawlerM::where('name', 'Contradictions-digi-' . $this->argument('rowId'))->where('type', 2)->orderBy('end', 'desc')->first();
-                if (isset($lastCrawler) AND !empty($lastCrawler)) {
-                    $startC = $lastCrawler->end + 1;
-                    $endC = $startC + CrawlerM::$crawlerSize;
-                    
-                } else {
-                    $startC = 1;
-                    $endC = $startC + CrawlerM::$crawlerSize;
-                }
+        try {
+            $newCrawler = CrawlerM::firstOrCreate(array('name' => 'Contradictions-digi-' . $this->argument('rowId'), 'start' => '1', 'end' => $check_count, 'status' => 1));
 
-                $this->info(" \n ---------- Check  " . $this->argument('rowId') . "     $startC  -> $endC         ------------ ");
-                $newCrawler = CrawlerM::firstOrCreate(array('name' => 'Contradictions-digi-' . $this->argument('rowId'), 'start' => $startC, 'end' => $endC, 'status' => 1, 'type' => 2));
-            } catch (\Exception $e) {
-                $this->info(" \n ---------- Check  " . $this->argument('rowId') . "              ------------ ");
-            }
+        } catch (\Exception $e) {
+            $this->info(" \n ---------- Check  " . $this->argument('rowId') . "              ------------ ");
         }
+
         if (isset($newCrawler)) {
-            $rowId = $startC;
-            while ($rowId <= $endC) {
+            $items = BookDigi::where('check_status', 0)->where('has_permit', 0)->get();
+            foreach ($items as $book_data) {
                 // bookirbook with ershad book
-                $book_data = BookDigi::where('id', $rowId)->first();
                 if (isset($book_data) and !empty($book_data)) {
-                    $update_data = array(
-                        'check_status' => 0,
-                        'has_permit' => 0,
-                    );
                     if ((isset($book_data->shabak) and $book_data->shabak != null and !empty($book_data->shabak))/* AND (isset($book_data->saleNashr) and $book_data->saleNashr != null and !empty($book_data->saleNashr))*/) {
                         $this->info($book_data->shabak);
                         $this->info($book_data->saleNashr);
@@ -113,11 +86,12 @@ class GetDigiContradictionsList extends Command
                         $update_data['has_permit'] = 4;
                     }
 
-                    BookDigi::where('id', $rowId)->update($update_data);
+                    BookDigi::where('id', $book_data->id)->update($update_data);
+                }
+                $newCrawler->last = $book_data->id;
+                $newCrawler->save();
                 }
 
-                CrawlerM::where('name', 'Contradictions-digi-' . $this->argument('rowId'))->where('start', $startC)->update(['last' => $rowId]);
-                $rowId++;
             }
         }
     }
