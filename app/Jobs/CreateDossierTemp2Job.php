eb5955434a0c910c34cfe24555743469350f4b65
Pouria Kazemi <pouriakazemi.php@gmail.com>
2024-11-03 13:37:41 +0330

    fix create temp dossier 2 command

--- app/Jobs/CreateDossierTemp2Job.php
+++ app/Jobs/CreateDossierTemp2Job.php
@@ -34,14 +34,19 @@ class CreateDossierTemp2Job implements ShouldQueue
      */
     public function handle()
     {
-        if($this->dossier->book_names[0] != (null or '')) {
+        if ($this->dossier->book_names[0] != (null or '')) {
             $data = [];
+
             try {
+                // Escape special characters in book name for regex
+                $escapedBookName = preg_quote($this->dossier->book_names[0], '/');
+
                 $newDossiers = BookTempDossier1::where('creator', $this->dossier->creator)
-                    ->where('book_names', 'all', [new \MongoDB\BSON\Regex($this->dossier->book_names[0], 'i')])
+                    ->where('book_names', 'all', [new \MongoDB\BSON\Regex($escapedBookName, 'i')])
                     ->where('_id', '!=', $this->dossier->_id)
                     ->get();
-            }catch (CommandException $e) {
+
+            } catch (CommandException $e) {
                 Log::error('Regex error in document search.', [
                     'dossier_id' => $this->dossier->_id,
                     'book_names' => $this->dossier->book_names,
@@ -49,16 +54,18 @@ class CreateDossierTemp2Job implements ShouldQueue
                 ]);
                 throw $e;
             }
+
             if (count($newDossiers) != 0) {
                 $data['creator'] = $this->dossier->creator;
                 $data['dossier_temp_one_id_original'] = $this->dossier->_id;
                 $data['book_names_original'] = $this->dossier->book_names;
+
                 foreach ($newDossiers as $dossier) {
-                    $data ['dossier_temp_one_id'][] = $dossier->_id;
+                    $data['dossier_temp_one_id'][] = $dossier->_id;
                     $data['book_names'][] = $dossier->book_names;
                 }
-                $count = count($data['book_names']);
-                $data['book_counts'] = $count;
+
+                $data['book_counts'] = count($data['book_names']);
                 BookTempDossier2::create($data);
             }
         }
