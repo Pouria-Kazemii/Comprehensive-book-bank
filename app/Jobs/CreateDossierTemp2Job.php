5de2b3ae954c22939bddd6ff386bfbe456b98693
Pouria Kazemi <pouriakazemi.php@gmail.com>
2024-11-03 11:50:21 +0330

    fix create dossier 2 command

--- app/Jobs/CreateDossierTemp2Job.php
+++ app/Jobs/CreateDossierTemp2Job.php
@@ -32,23 +32,24 @@ class CreateDossierTemp2Job implements ShouldQueue
      */
     public function handle()
     {
-        $data = [];
-        $newDossiers = BookTempDossier1::where('creator' , $this->dossier->creator)
-            ->where('book_names' , 'all',[new \MongoDB\BSON\Regex($this->dossier->book_names[0], 'i')])
-            ->where('_id','!=',$this->dossier->_id)
-            ->get();
-        if (count($newDossiers) != 0) {
-            $data['creator'] = $this->dossier->creator;
-            $data['dossier_temp_one_id_original'] = $this->dossier->_id;
-            $data['book_names_original'] = $this->dossier->book_names;
-            foreach ($newDossiers as $dossier){
-                $data ['dossier_temp_one_id'][] = $dossier->_id;
-                $data['book_names'][] = $dossier->book_names;
+        if($this->dossier->book_names[0] != (null or '')) {
+            $data = [];
+            $newDossiers = BookTempDossier1::where('creator', $this->dossier->creator)
+                ->where('book_names', 'all', [new \MongoDB\BSON\Regex($this->dossier->book_names[0], 'i')])
+                ->where('_id', '!=', $this->dossier->_id)
+                ->get();
+            if (count($newDossiers) != 0) {
+                $data['creator'] = $this->dossier->creator;
+                $data['dossier_temp_one_id_original'] = $this->dossier->_id;
+                $data['book_names_original'] = $this->dossier->book_names;
+                foreach ($newDossiers as $dossier) {
+                    $data ['dossier_temp_one_id'][] = $dossier->_id;
+                    $data['book_names'][] = $dossier->book_names;
+                }
+                $count = count($data['book_names']);
+                $data['book_counts'] = $count;
+                BookTempDossier2::create($data);
             }
-            $count = count($data['book_names']);
-            $data['book_counts'] = $count;
-            BookTempDossier2::create($data);
         }
-
     }
 }
